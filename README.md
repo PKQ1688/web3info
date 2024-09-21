a9cf4d1ea0fedac45920583a6685b9b4dfd6ab18
adolf <adolf1321794021@gmail.com>
2024-09-21 17:15:57 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # web3info
+
 intelligently access everything you want to know about Web3.
--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,16 @@
-use std::env::set_var;
+static MY_STATIC: i32 = 42;
 
-use agents::MyAgent;
-use axum::{
-    extract::State,
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, post},
-    Json, Router,
-};
+fn main() {
+    const SECOND_HOUR: usize = 3600;
+    const SECOND_DAY: usize = SECOND_HOUR * 24;
 
-// use qdrant_client::Qdrant;   
-use qdrant_client::client::QdrantClient;
-use serde::Deserialize;
+    println!("{} ", SECOND_DAY);
 
-pub mod agents;
-pub mod files;
-
-use files::File;
-use shuttle_runtime::SecretStore;
-
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
-
-#[derive(Deserialize)]
-pub struct Prompt {
-    prompt: String,
-}
-
-#[derive(Clone)]
-pub struct AppState {
-    agent: MyAgent,
-}
-
-async fn prompt(State(state): State<AppState>, Json(json): Json<Prompt>) -> impl IntoResponse {
-    match state.agent.prompt(&json.prompt).await {
-        Ok(res) => (StatusCode::OK, res),
-        Err(e) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {e}"),
-        ),
+    {
+        const SE: usize = 1_000;
+        println!("{} ", SE);
     }
-}
 
-#[shuttle_runtime::main]
-async fn main(
-    #[shuttle_qdrant::Qdrant(
-        cloud_url = "{secrets.QDRANT_URL}",
-        api_key = "{secrets.QDRANT_API_TOKEN}"
-    )]
-    // qdrant_client: Qdrant,
-    qdrant_client: qdrant_client::client::QdrantClient,
-    #[shuttle_runtime::Secrets] secrets: SecretStore,
-) -> shuttle_axum::ShuttleAxum {
-    secrets.into_iter().for_each(|x| {
-        set_var(x.0, x.1);
-    });
-
-    let file = File::new("test.csv".into())?;
-
-    let state = AppState {
-        agent: MyAgent::new(qdrant_client),
-    };
-
-    state.agent.embed_document(file).await?;
-
-    let router = Router::new()
-        .route("/", get(hello_world))
-        .route("/prompt", post(prompt))
-        .with_state(state);
-
-    Ok(router.into())
+    println!("{} ", SECOND_HOUR);
+    println!("{MY_STATIC} ");
 }
